05b98d6421379c8fdf7eff88b2002a1405cee784
phrush <w.phrush@gmail.com>
2017-04-21 10:26:28 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+const router = function ($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'home.html',
+    })
+    .when('/search', {
+      templateUrl: 'search.html',
+    })
+    .otherwise({
+      redirectTo: '/home',
+    });
+};
+
+angular.module('ali-gle', ['ngRoute', 'main-Fact', 'main-Ctrl'])
+.config(router);
+
+angular.module('main-Fact', [])
+.factory('mainFact', function() {
+    const quotes = [
+      "What does the error say?",
+      "Did you Google it?",
+      "Google it.",
+      "What does your console say?",
+      "Someone probably had that issue on Stack Overflow!",
+      "Come on,\rthat's so easy!",
+      "Very well.",
+      "There's a million ways to skin a cat!"
+    ];
+
+    const search = () => {
+      var cx = '015179888997701780015:mucxlpfvmtc';
+      var gcse = document.createElement('script');
+      gcse.type = 'text/javascript';
+      gcse.async = true;
+      gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(gcse, s);
+    };
+
+    const randomQuote = () => {
+        const max = quotes.length - 1;
+        const index = Math.floor(Math.random() * max);
+        return quotes[index];
+    }
+
+    return {
+      search,
+      randomQuote,
+    };
+})
+
+angular.module('main-Ctrl', [])
+.controller('mainCtrl', function($scope, $location, mainFact) {
+    if (!$scope.quote) {
+      $location.path('home');
+    };
+    
+    $scope.bug = null;
+    $scope.quote = null;
+    $scope.answer = false;
+
+    $scope.test = (input) => {
+        console.log(input);
+    };
+
+    $scope.searchBox = () => {
+      mainFact.search();
+    };
+
+    $scope.ask = (question) => {
+        $scope.quote = mainFact.randomQuote();
+        $scope.answer = true;
+        $location.path('search');
+    };
+
+    $scope.reset = () => {
+        $scope.answer = false;
+        $location.path('home');
+    };
+});
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+require('dotenv').config();
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+
+console.log(PORT);
+
+app.use(express.static(path.join(__dirname, '/')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+  console.log(`listening at http://${HOST}:${PORT}`);
+});
