c46733306e3023b661df650a6082a25c19de5edf
Olivia Sabo-Rush <w.phrush@gmail.com>
2017-10-25 23:04:45 -0500

    add chrome extension version

---
+++ chrome-extension/gcse.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = '015179888997701780015:mucxlpfvmtc';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
